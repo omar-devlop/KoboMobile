7c78eb54e022909fabbcc904b4d99a9f2b76e6c2
omar-devlop <omaralafadouma@gmail.com>
2025-03-29 15:07:18 +0300

    small fixes

--- lib/featuers/users/screen/users_screen.dart
+++ lib/featuers/users/screen/users_screen.dart
@@ -1,9 +1,11 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:kobo/core/helpers/extensions.dart';
 import 'package:kobo/core/utils/routing/routes.dart';
 import 'package:kobo/featuers/users/bloc/cubit/users_cubit.dart';
+import 'package:kobo/featuers/users/widget/user_card.dart';
 
 class UsersScreen extends StatelessWidget {
   const UsersScreen({super.key});
@@ -11,37 +13,101 @@ class UsersScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     ThemeData theme = Theme.of(context);
+    Size screenSize = MediaQuery.of(context).size;
+
+    navigateToLoginScreen() {
+      if (context.mounted) {
+        context.pushNamedAndRemoveUntil(
+          Routes.loginScreen,
+          predicate: (Route<dynamic> route) => false,
+        );
+      }
+    }
+
+    clearAllSavedAccounts() async {
+      await BlocProvider.of<UsersCubit>(context).clearSavedUsers();
+      navigateToLoginScreen();
+    }
 
     getUsersList(List<String> data, {String? loadingUser}) {
       return Padding(
-        padding: const EdgeInsets.symmetric(vertical: 48.0, horizontal: 24),
+        padding: const EdgeInsets.symmetric(horizontal: 24),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          spacing: 12.0,
           children: [
+            SizedBox(height: 72),
+            SvgPicture.asset(
+              'assets/svg/kobo_logo.svg',
+              width: screenSize.width / 1.5,
+            ),
+            SizedBox(height: 48),
             Row(
               mainAxisSize: MainAxisSize.max,
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text(
-                  context.tr("users"),
+                  context.tr("savedAccounts"),
                   style: theme.textTheme.titleLarge?.copyWith(
                     fontWeight: FontWeight.bold,
                   ),
                 ),
                 TextButton.icon(
-                  onPressed: () async {
-                    await BlocProvider.of<UsersCubit>(
-                      context,
-                    ).clearSavedUsers();
-                    WidgetsBinding.instance.addPostFrameCallback((_) {
-                      context.pushNamedAndRemoveUntil(
-                        Routes.loginScreen,
-                        predicate: (Route<dynamic> route) => false,
-                      );
-                    });
+                  onPressed: () {
+                    showDialog<String>(
+                      context: context,
+                      builder:
+                          (BuildContext context) => Dialog(
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 20.0,
+                                vertical: 12,
+                              ),
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.stretch,
+                                children: <Widget>[
+                                  const SizedBox(height: 10),
+                                  Text(
+                                    context.tr("clearAll"),
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 10),
+                                  Text(context.tr('areYouSure')),
+                                  const SizedBox(height: 10),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      TextButton.icon(
+                                        onPressed: context.pop,
+                                        label: Text(context.tr('cancel')),
+                                        icon: Icon(Icons.close),
+                                      ),
+                                      TextButton.icon(
+                                        style: TextButton.styleFrom(
+                                          iconColor: theme.colorScheme.error,
+                                          foregroundColor:
+                                              theme.colorScheme.error,
+                                        ),
+                                        onPressed: clearAllSavedAccounts,
+                                        label: Text(context.tr("clearAll")),
+                                        icon: Icon(Icons.delete_outline),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                    );
                   },
                   label: Text(context.tr('clearAll')),
-                  icon: Icon(Icons.close),
+                  icon: Icon(Icons.delete_outline),
                   style: TextButton.styleFrom(
                     iconColor: theme.colorScheme.error,
                     foregroundColor: theme.colorScheme.error,
@@ -51,8 +117,56 @@ class UsersScreen extends StatelessWidget {
             ),
             Expanded(
               child: ListView.builder(
-                itemCount: data.length,
+                itemCount: data.length + 1,
                 itemBuilder: (context, index) {
+                  if (index == data.length) {
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 8.0),
+                          child: Divider(),
+                        ),
+                        GestureDetector(
+                          onTap: navigateToLoginScreen,
+                          child: Container(
+                            margin: EdgeInsets.symmetric(vertical: 8.0),
+                            padding: EdgeInsets.all(12.0),
+                            decoration: BoxDecoration(
+                              color: theme.colorScheme.surfaceContainer,
+                              borderRadius: BorderRadius.circular(4.0),
+                            ),
+                            child: Row(
+                              spacing: 12.0,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              mainAxisSize: MainAxisSize.max,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Icon(
+                                  Icons.person_add,
+                                  color: theme.colorScheme.primary,
+                                ),
+                                Expanded(
+                                  child: Text(
+                                    context.tr("addAccount"),
+                                    overflow: TextOverflow.ellipsis,
+                                    style: theme.textTheme.labelLarge!.copyWith(
+                                      color: theme.colorScheme.primary,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                                Icon(
+                                  Icons.add,
+                                  color: theme.colorScheme.primary,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    );
+                  }
                   return UserCard(user: data[index], loadingUser: loadingUser);
                 },
               ),
@@ -73,12 +187,7 @@ class UsersScreen extends StatelessWidget {
       body: BlocConsumer<UsersCubit, UsersState>(
         listener: (context, state) {
           if (state is Empty) {
-            WidgetsBinding.instance.addPostFrameCallback((_) {
-              context.pushNamedAndRemoveUntil(
-                Routes.loginScreen,
-                predicate: (Route<dynamic> route) => false,
-              );
-            });
+            navigateToLoginScreen();
           }
         },
 
@@ -88,89 +197,11 @@ class UsersScreen extends StatelessWidget {
 
             savedUsers: (data) => getUsersList(data),
 
-            empty: () => Center(child: Text(context.tr("noSavedUsers"))),
+            empty: () => getUsersList([]),
             logging:
                 (data, userName) => getUsersList(data, loadingUser: userName),
           );
         },
-      ),
-      floatingActionButton: FloatingActionButton(
-        tooltip: context.tr("addAccount"),
-        onPressed:
-            () => context.pushNamedAndRemoveUntil(
-              Routes.loginScreen,
-              predicate: (Route<dynamic> route) => false,
-            ),
-        child: Icon(Icons.person_add),
-      ),
-    );
-  }
-}
-
-class UserCard extends StatelessWidget {
-  final String user;
-  final String? loadingUser;
-  const UserCard({super.key, required this.user, this.loadingUser});
-
-  @override
-  Widget build(BuildContext context) {
-    bool isUserLoading = loadingUser != null && (user == loadingUser);
-
-    ThemeData theme = Theme.of(context);
-    return GestureDetector(
-      onTap:
-          loadingUser != null
-              ? null
-              : () async {
-                bool isAuth = await BlocProvider.of<UsersCubit>(
-                  context,
-                ).savedUserLogin(userName: user);
-                if (isAuth) {
-                  debugPrint("isAuth : $isAuth | $isUserLoading");
-                  WidgetsBinding.instance.addPostFrameCallback((_) {
-                    context.pushNamedAndRemoveUntil(
-                      Routes.homeScreen,
-                      predicate: (Route<dynamic> route) => false,
-                    );
-                  });
-                }
-              },
-      child: Container(
-        margin: EdgeInsets.symmetric(vertical: 8.0),
-        padding: EdgeInsets.all(12.0),
-        decoration: BoxDecoration(
-          color:
-              isUserLoading
-                  ? theme.colorScheme.secondaryContainer
-                  : theme.colorScheme.surfaceContainer, // surfaceContainerLow
-          borderRadius: BorderRadius.circular(4.0),
-        ),
-        child: Row(
-          spacing: 12.0,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          mainAxisSize: MainAxisSize.max,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Icon(Icons.person),
-            Expanded(
-              child: Text(
-                user,
-                overflow: TextOverflow.ellipsis,
-                style: theme.textTheme.labelLarge!.copyWith(
-                  color: theme.colorScheme.onSecondaryContainer,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            isUserLoading
-                ? SizedBox(
-                  width: 20,
-                  height: 20,
-                  child: CircularProgressIndicator(strokeCap: StrokeCap.round),
-                )
-                : Icon(Icons.chevron_right),
-          ],
-        ),
       ),
     );
   }
